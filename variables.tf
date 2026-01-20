175c45cd07d1e1d3601cbef6d4356069b8f73f76
Arshad Khan <ArshadKhan627@outlook.com>
2026-01-20 18:49:55 +0530

    Added comment

--- variables.tf
+++ variables.tf
@@ -18,3 +18,4 @@ variable "ec2_ami_id" {
     type = string
 }
 
+#Notes: Variables are used to store values that can be used in the configuration. They are defined in a variables.tf file. Used to avoid hardcode values in the configuration.Using variables makes the code more maintainable and reusable.
