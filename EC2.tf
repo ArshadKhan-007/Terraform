c3edf7e3b156c55215642e8b5c7ff87ff70cdd60
Arshad Khan <ArshadKhan627@outlook.com>
2026-01-26 11:17:26 +0530

    Used count keyword to create multiple instances.

--- EC2.tf
+++ EC2.tf
@@ -7,8 +7,8 @@
 # Notes: Before creating any resource make sure to check that have access key and secret key configured.
 
 resource "aws_key_pair" "deployer" {
-    key_name = "terraec2-key"
-    public_key = file("terraec2-key.pub")
+  key_name   = "terraec2-key"
+  public_key = file("terraec2-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
@@ -16,54 +16,55 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "My security group"
+  vpc_id      = aws_default_vpc.default.id
+
+  #inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic"
+  }
+
+  #outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
     name = "my-security-group"
-    description = "My security group"
-    vpc_id = aws_default_vpc.default.id
-
-    #inbound rules
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow HTTP traffic"
-    }
-
-    #outbound rules
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow all outbound traffic"
-    }
-
-    tags = {
-      name = "my-security-group"
-    }
+  }
 }
 
 #ec2 instance creation
 resource "aws_instance" "Server" {
-    key_name = aws_key_pair.deployer.key_name
-    security_groups = [aws_security_group.my_security_group.name]
-    instance_type = var.ec2_instance_type
-    ami = var.ec2_ami_id
-    user_data = file("install_nginx.sh")
+  count = 2 #count is a meta argument that allows you to create multiple instances without copy and paste the same code
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.my_security_group.name]
+  instance_type   = var.ec2_instance_type
+  ami             = var.ec2_ami_id
+  user_data       = file("install_nginx.sh")
 
-    root_block_device {
-      volume_size = var.ec2_root_storage_size
-      volume_type = var.ec2_root_storage_type
-    }
+  root_block_device {
+    volume_size = var.ec2_root_storage_size
+    volume_type = var.ec2_root_storage_type
+  }
 
-    tags = {
-      name = "Server"
-    }
+  tags = {
+    name = "Server"
+  }
 }
