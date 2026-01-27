96a31a86235a1a646113e4ac02beba0d3f5c1de6
Arshad Khan <ArshadKhan627@outlook.com>
2026-01-27 11:22:41 +0530

    Initial Commit

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,26 @@
-#Notes: Outputs block are used to display information about the resources on the terminal.
+# #Notes: Outputs block are used to display information about the resources on the terminal.
+
+# output "ec2_public_ip" {
+#   value = aws_instance.Server[*].public_ip # Here [*] is used to display all the public ips of the instances
+# }
+
+# output "ec2_public_dns" {
+#   value = aws_instance.Server[*].public_dns # Here [*] is used to display all the public dns of the instances
+# }
+
+# output "ec2_private_ip" {
+#   value = aws_instance.Server[*].private_ip # Here [*] is used to display all the private ips of the instances
+# }
 
 output "ec2_public_ip" {
-  value = aws_instance.Server[*].public_ip # Here [*] is used to display all the public ips of the instances
+  value = [
+    for instance in aws_instance.Server: instance.public_ip
+  ]
 }
 
-output "ec2_public_dns" {
-  value = aws_instance.Server[*].public_dns # Here [*] is used to display all the public dns of the instances
-}
-
-output "ec2_private_ip" {
-  value = aws_instance.Server[*].private_ip # Here [*] is used to display all the private ips of the instances
-}
-
+# Notes: Whenever you're using for_each you should use for loop to display the outputs.
+# Use count with for_each is not allowed.
+# Use count when you want to create multiple resources with the same configuration.
+# Use for_each when you want to create multiple resources with different configurations.
+# tomap() function is used to convert a list to a map.
+# toset() function is used to convert a list to a set.
